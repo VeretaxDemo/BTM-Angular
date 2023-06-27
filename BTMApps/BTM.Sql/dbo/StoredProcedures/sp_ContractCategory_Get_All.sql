f9e9572f82a8c31dac14160226f04721f04e3d00
westerntc <veretax@gmail.com>
2023-06-27 09:31:10 -0400

    Added Stored Procedures to Get All ContractCategory and ContractType

---
+++ BTMApps/BTM.Sql/dbo/StoredProcedures/sp_ContractCategory_Get_All.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[sp_ContractCategory_Get_All]
+AS
+BEGIN
+    SET NOCOUNT ON;
+	SELECT [Id]
+      ,[Title]
+      ,[Description]
+      ,[SpecialConsiderations]
+  FROM [dbo].[ContractCategory]
+END
+RETURN 0
---
+++ BTMApps/BTM.Sql/dbo/StoredProcedures/sp_ContractType_Get_All.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[sp_ContractType_Get_All]
+AS
+BEGIN
+    SET NOCOUNT ON;
+	SELECT [Id]
+      ,[ContractCategoryId]
+      ,[Title]
+      ,[Description]
+  FROM [dbo].[ContractTypes]
+END
+RETURN 0
