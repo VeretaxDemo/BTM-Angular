b84e711503aff60bbc834d4d63eeb826530d49e7
westerntc <veretax@gmail.com>
2023-07-08 22:30:52 -0400

    Setup new project just for clearner setup of EntityFramework Initially only have contractcategoryservice and AppDbContext

---
+++ BTMApps/Btm.Api.Library.Data.EntityFrameworkCore/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Btm.Api.Data.Models.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Btm.Api.Library.Data.EntityFrameworkCore.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<ContractCategory> ContractCategory { get; set; }
+    public DbSet<ContractType> ContractTypes { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure your entity mappings or relationships here
+        // Example: modelBuilder.Entity<ContractCategory>().HasKey(c => c.Id);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ BTMApps/Btm.Api.Library.Data.EntityFrameworkCore/Services/ContractCategoryService.cs
@@ -0,0 +1,21 @@
+﻿using Btm.Api.Data.Models.Contracts;
+using Btm.Api.Data.Services;
+using Btm.Api.Library.Data.EntityFrameworkCore.Context;
+
+namespace Btm.Api.Library.Data.EntityFrameworkCore.Services;
+
+public class ContractCategoryService : IContractCategoryService
+{
+
+    private readonly AppDbContext _dbContext;
+    public ContractCategoryService(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public List<ContractCategory> GetAllContractCategories()
+    {
+        List<ContractCategory> contractCategories = _dbContext.ContractCategory.ToList();
+        return contractCategories;
+    }
+}
