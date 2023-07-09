9915987ca08f22712ef73bd7d063519e4078127b
westerntc <veretax@gmail.com>
2023-07-08 20:49:18 -0400

    NetworkConnectivityChecker Added to vet connection to Docker

---
+++ BTMApps/NetworkConnectivityChecker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+string server = "localhost";
+int port = 14333;
+string ukey = "BTM_AdminOnlyUser";
+string pwd = "rvb29gaqrU!G7CC6*v2gxUKfW";
+string connectionString = $"Server={server},{port};Database=master;User Id={ukey};Password={pwd};";
+
+try
+{
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        Console.WriteLine("Connection successful!");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Connection failed: {ex.Message}");
+}
